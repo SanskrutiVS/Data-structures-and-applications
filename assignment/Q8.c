7bd9a863d597627ccb4243047827df311a0a954c
SanskrutiVS <83571533+SanskrutiVS@users.noreply.github.com>
2022-03-20 08:57:44 +0530

    Update Q8.c

--- assignment/Q8.c
+++ assignment/Q8.c
@@ -1,3 +1,12 @@
+//8)	A middle class family would always like to spend the money wisely, because of their financial status. They can’t afford to purchase the luxurious items unlike the Rich Class family. Even if they do, they do it very occasionally.  In order to keep track of the expenditure of the entire year, the middle class family maintains a record of how much they spent every month of that year.
+
+Assume that for the year 2020, the family maintains a record of expenditure for each month. At the end of the year, the family would like to know followings about their expenditure. 
+
+i)	Which month of the year they spent more (display month number)
+
+ii)	Which month of the year they spent less(display month number)
+iii)	Which are the months they spent more than 35000 rupees?(display month number)
+iv)	Calculate the average expenditure of the family for the year.
 #include <stdio.h>
 #include <stdlib.h>
 
