48ce75bd811c933858bc674790ae3e4ee94b2944
SanskrutiVS <83571533+SanskrutiVS@users.noreply.github.com>
2022-03-20 09:03:54 +0530

    Update Q14.c

--- assignment/Q14.c
+++ assignment/Q14.c
@@ -1,3 +1,8 @@
+//14)	Find equilibrium index in an array. An index is equilibrium, where sum of right side elements is equal to sum of left side elements.
+
+Ex: A={-7,1,5,2,-4,3,0} Here '3' is equilibrium index since A[0]+A[1]+A[2] = A[4]+A[5]+A[6].
+
+
 #include<stdio.h>
 void read(int n,int a[])
 {
